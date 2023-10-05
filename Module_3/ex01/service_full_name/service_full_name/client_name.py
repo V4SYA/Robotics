09680a53c6dd4b462ce34f8ef32e06a762ba1e6d
V4SYA <62791870+V4SYA@users.noreply.github.com>
2023-10-05 22:26:08 +0700

    Update client_name.py

--- Module_3/ex01/service_full_name/service_full_name/client_name.py
+++ Module_3/ex01/service_full_name/service_full_name/client_name.py
@@ -14,17 +14,20 @@ class ClientName(Node):
         # выводится сообщение "I can't wait any more"
         while not self.client.wait_for_service(timeout_sec=1.0):
         	self.get_logger().info('I can't wait any more')
-        
+         
         # объект запроса для сервиса, 
         # который будет использоваться для отправки запроса сервису
         self.request = FullNameSumService.Request()
-        
+
+    # принимаем аргументы last_name, name и first_name, 
+    # устанавливаем их в объекте запроса, а затем вызываем сервис асинхронно
     def give_request(self, last_name, name, first_name):
         self.request.last_name = last_name
         self.request.name = name
         self.request.first_name = first_name
         
         res = self.client.call_async(self.request)
+        # ожидаем завершения асинхронного вызова сервиса
         rclpy.spin_until_future_complete(self, res)
         
         return res.result()
@@ -33,7 +36,8 @@ def main():
     rclpy.init()
     client_name = ClientName()
     res = client_name.give_request(sys.argv[1], sys.argv[2], sys.argv[3])
-    
+
+    # если получен результат, выводим полное имя
     if res:
     	client_name.get_logger().info('Full name: %s' % res.full_name)
         
