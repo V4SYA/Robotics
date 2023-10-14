396b09a4c1d25bd0b68889757de3dc8fe711f625
V4SYA <62791870+V4SYA@users.noreply.github.com>
2023-10-14 12:24:19 +0700

    Update service_name.py

--- Module_3/ex01/service_full_name/service_full_name/service_name.py
+++ Module_3/ex01/service_full_name/service_full_name/service_name.py
@@ -2,19 +2,15 @@ import rclpy
 from rclpy.node import Node
 from ex09_package.srv import FullNameSumService
 
+
 class ServiceName(Node):
 
     def __init__(self):
         super().__init__('service_name')
-        
-        # Создаём сервер сервиса FullNameSumService с именем "summ_full_name"
-        # и указываем метод handle_service_request для обработки запросов.
         self.service = self.create_service(
         FullNameSumService, 'summ_full_name', self.handle_service_request
         )
 
-    # Принимаем запрос request и объект response, обрабатываем запрос,
-    # формируем полное имя и устанавливаем его в объекте ответа:
     def handle_service_request(self, request, response):
         last_name = request.last_name
         name = request.name
@@ -23,17 +19,19 @@ class ServiceName(Node):
         full_name = f"{last_name} {name} {first_name}"
         response.full_name = full_name
         
+        self.get_logger().info('Full name: %s' % full_name)
+        
         return response
-
+    	
 def main(args=None):
     rclpy.init(args=args)
 
-    # Создали экземпляр
     minimal_service = ServiceName()
 
-    # Запуск сервера и ожидание запросов
     rclpy.spin(minimal_service)
+
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
