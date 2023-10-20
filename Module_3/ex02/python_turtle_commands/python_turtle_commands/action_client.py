6fd5b31445697f78b8685d10c209251bddb48410
V4SYA <62791870+V4SYA@users.noreply.github.com>
2023-10-21 00:49:48 +0700

    Update action_client.py

--- Module_3/ex02/python_turtle_commands/python_turtle_commands/action_client.py
+++ Module_3/ex02/python_turtle_commands/python_turtle_commands/action_client.py
@@ -23,27 +23,27 @@ class CommandsActionClient(Node):
         goal_msg.s = s
         goal_msg.angle = angle
 
-	# Ожидаем, когда сервер action станет доступным
+        # Ожидаем, когда сервер action станет доступным
         self._action_client.wait_for_server()
-        
+
         # Отправляем асинхронную цель на сервер и устанавливаем feedback_callback для обработки обратной связи
         self._send_goal_future = self._action_client.send_goal_async(goal_msg,feedback_callback=self.feedback_callback)
-	
-	# Добавляем обратный вызов для обработки ответа от сервера на goal
+
+        # Добавляем обратный вызов для обработки ответа от сервера на goal
         self._send_goal_future.add_done_callback(self.goal_response_callback)
 
     # Метод для обработки ответа от сервера на goal
     def goal_response_callback(self, future):
-    
+
         # Получаем результат обработки goal
         goal_handle = future.result()
-        
+
         # Проверяем: принята или нет
         if not goal_handle.accepted:
-            self.get_logger().info('Goal is rejected')
+            self.get_logger().info('Goal rejected')
             return
 
-	# Дошли до этого момента - задача была принята, выводим соответ. сообщение и начинаем ждать результат
+        # Дошли до этого момента - задача была принята, выводим соответ. сообщение и начинаем ждать результат
         self.get_logger().info('Goal is accepted')
 
         self._get_result_future = goal_handle.get_result_async()
@@ -53,12 +53,12 @@ class CommandsActionClient(Node):
     def get_result_callback(self, future):
         # Получаем результат и выводим соответ. сообщение
         result = future.result().result
-        self.get_logger().info('The path is traveled: {0}'.format(result.result))
+        self.get_logger().info('Result: {0}'.format(result.result))
         
         global goals_for_turtle
         # Удаляем первую задачу, считая, что она была выполнена
         goals_for_turtle.pop(0)
-        
+
         # Если в списке задач остались задачи, отправляем следующую черепахе, иначе завершает работу ROS
         if goals_for_turtle: self.send_goal(*goals_for_turtle[0])
         else: rclpy.shutdown()
@@ -69,18 +69,22 @@ class CommandsActionClient(Node):
         feedback = feedback_msg.feedback
 
 
+
 def main(args=None):
     # Инициализируем ROS 2 с переданными аргументами
-    rclpy.init(args = args)
+    rclpy.init(args=args)
 
     action_client = CommandsActionClient()
     
     global goals_for_turtle
     
     # Добавляем задачи
-    goals_for_turtle.append([COMMAND_FORWARD, 2, 0])
     goals_for_turtle.append([COMMAND_RIGHT, 0, 90])
-    goals_for_turtle.append([COMMAND_FORWARD, 1, 0])
+    goals_for_turtle.append([COMMAND_FORWARD, 2, 0])
+    goals_for_turtle.append([COMMAND_LEFT, 0, 90])
+    goals_for_turtle.append([COMMAND_FORWARD, 4, 0])
+    goals_for_turtle.append([COMMAND_LEFT, 0, 90])
+    goals_for_turtle.append([COMMAND_FORWARD, 5, 0])
     
     # Отправили первую задачу
     action_client.send_goal(*goals_for_turtle[0])
